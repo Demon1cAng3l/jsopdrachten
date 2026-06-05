901eb611ed6092a8865a6b7077ba971d8075c180
Demon1cAng3l <wolfy240607@gmail.com>
2026-06-05 09:14:33 +0200

    Hoofdstuk 3 af saba.js

--- jsfiles/saba.js
+++ jsfiles/saba.js
@@ -1,6 +1,6 @@
 document.querySelector('#box1').style.backgroundColor = "green";
 document.querySelector('#box2').style.backgroundColor = "orange";
-document.querySelector('#box3').style.backgroundColor = "fuchsia";
+document.querySelector('#box3').style.backgroundColor = "cyan";
 document.querySelector('#box4').style.backgroundColor = "azure";
 document.querySelector('body').style.backgroundColor = "black"
 
@@ -8,5 +8,31 @@ document.querySelector('#box1').addEventListener('click', function() {
     document.querySelector("#box2").style.backgroundColor = "rebeccapurple";
 });
 document.querySelector('#box2').addEventListener('click', function() {
-    document.querySelector("#box1").style.backgroundColor = "mediumorchid";
+    document.querySelector("#box1").style.backgroundColor = "fuchsia";
+    console.log(box1.naam)
+});
+
+let auto = {
+    "merk": "Nissan",
+    "model": "Sylvia S15",
+    "bouwjaar": 1999,
+    "kleuren": ["rood", "zwart", "wit"]
+};
+
+document.querySelector("#box2").addEventListener("click", function () {
+    document.querySelector("#box1").innerHTML =
+        `<h1>${auto.merk} ${auto.model}</h1>
+         <p>Bouwjaar: ${auto.bouwjaar}</p>`;
+});
+
+document.querySelector("#box1").addEventListener("click", function () {
+
+    let kleurenHTML = "";
+    auto.kleuren.forEach(function (kleur) {
+        kleurenHTML += `<li>${kleur}</li>`;
+    });
+
+    document.querySelector("#box2").innerHTML =
+        `<h1>Kleuren beschikbaar</h1>
+         <ul>${kleurenHTML}</ul>`;
 });
